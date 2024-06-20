8b9fab509ba25e5ca07707a86f4f97cdef55e34f
Daulet Kenzhetayev <daulet.kenzhetayev@nu.edu.kz>
2024-06-20 08:41:47 +0100

    Package

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "TestingSDK",
+    platforms: [
+        .macOS(.v12), .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "TestingSDK",
+            targets: [
+                "TestingCoreSDK",
+                "TestingUISDK"
+            ]
+        )
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "TestingCoreSDK",
+            url: "https://github.com/testing/core",
+            checksum: "f329505fdbf1561d6080fe45560319405787c064e0cf9a487477f247dac7772w"
+        ),
+        .binaryTarget(
+            name: "TestingUISDK",
+            url: "https://github.com.testing/ui",
+            checksum: "f329505fdbf1561d6080fe45560319405787c064e0cf9a487477f247dac7771q"
+        )
+    ]
+)
